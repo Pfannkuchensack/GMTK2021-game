f615af297ebabbbf3e0ac71f260c13e8066295a0
Alexander Eichhorn <alex@code-with.us>
2021-06-12 00:30:44 +0200

    beispiel

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,57 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col">
+        <div
+          id="bestellungen"
+          v-for="bestellung in this.bestellungen"
+          :key="bestellung.id"
+        >
+		<img :src="bestellung.icon" height="100px" width="100px"/>
+		</div>
+		<button @click="addBestellung()" name="add" value="add">Add</button>
+		<button @click="deleteBestellung()" name="delete" value="delete">delete</button>
+		<button @click="stopBestellung()" name="stop" value="stop">stop</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+		name: "",
+		inheritAttrs: false,
+		props: {
+		},
+		data: () => {
+			return {
+			bestellungen: [],
+			cnt: 1,
+			intervalid1: false,
+			}
+		},
+		methods:
+		{
+			addBestellung()
+			{
+				this.cnt = this.cnt+1;
+				this.bestellungen.push({'id': this.cnt, 'name': 'schwert', 'icon': 'https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836'});
+			},
+			deleteBestellung()
+			{
+				this.bestellungen.shift();
+			},
+			stopBestellung()
+			{
+				clearInterval(this.intervalid1);
+				//clearTimeout((this.intervalid1);
+			},
+		},
+		mounted() {
+				console.log("Mounted");
+				this.intervalid1 = setInterval(function(){
+					this.addBestellung()
+				}.bind(this), 5000);
+			}
+	}
 </script>
