41b8b02bc84902b25ea77c318ff504baf991846b
Michael Matthé <michael.matthe@gmail.com>
2021-06-12 20:00:55 +0200

    updates

--- resources/js/components/game.vue
+++ resources/js/components/game.vue
@@ -1,131 +1,147 @@
 <template>
-  <div>
-    <orders :ordersProp="orders"></orders>
-    <gamefield :playerXProp="playerX" :playerYProp="playerY" :playerIventoryProp="playerIventory"></gamefield>
-  </div>
+    <div>
+        <orders :ordersProp="orders"></orders>
+        <gamefield
+            :playerXProp="playerX"
+            :playerYProp="playerY"
+            :playerInventoryProp="playerIventory"
+        ></gamefield>
+    </div>
 </template>
 
 <script>
 export default {
-  components: {},
-  name: "game",
-  inheritAttrs: false,
-  props: {},
-  data: () => {
-    return {
-      orders: [],
-      cnt: 0,
-      settimeout: false,
-      playerX: 0,
-      playerY: 0,
-	  playerIventory: 0,
-      itemstypes: [
-		{ type: 0, name: "Nix" },
-        { type: 1, name: "Eisen" },
-        { type: 2, name: "schmelzendes Eisen" },
-        { type: 3, name: "Geschmolzendes Eisen" },
-        { type: 4, name: "zerschmolzendes Eisen" },
-        { type: 10, name: "Holz" },
-        { type: 11, name: "Schwert Schaft" },
-        { type: 12, name: "Axe Schaft" },
-        { type: 13, name: "Harke Schaft" },
-        { type: 14, name: "Tool Schaft" },
-        { type: 20, name: "Schwertklinge" },
-        { type: 21, name: "Axeklinge" },
-        { type: 22, name: "Hammerkopf" },
-        { type: 23, name: "Harkekopf" },
-      ],
-      ordertypes: [
-        { type: 100, type1: 20, type2: 11, name: "Schwert", time: 2000 },
-        { type: 101, type1: 21, type2: 12, name: "Axe", time: 4000 },
-        { type: 102, type1: 22, type2: 14, name: "Hammer", time: 1000 },
-        { type: 103, type1: 23, type2: 13, name: "Harke", time: 3000 },
-      ],
-      icons: [
-        {
-          name: "Schwert",
-          icon: "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
-        },
-        {
-          name: "Axe",
-          icon: "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
-        },
-        {
-          name: "Hammer",
-          icon: "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
-        },
-        {
-          name: "Harke",
-          icon: "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
-        },
-      ],
-    };
-  },
-  methods: {
-    addorder() {
-      this.cnt = this.cnt + 1;
-      let tmp = _.sample(this.ordertypes); //[Math.floor(Math.random()*this.ordertypes.length) | 0];
-      tmp.id = this.cnt;
-      tmp.icon = this.icons.find((element) => element.name == tmp.name).icon;
-      this.orders.push(Object.assign({}, tmp));
-      return Object.assign({}, tmp);
+    components: {},
+    name: "game",
+    inheritAttrs: false,
+    props: {},
+    data: () => {
+        return {
+            orders: [],
+            cnt: 0,
+            settimeout: false,
+            playerX: 0,
+            playerY: 0,
+            playerIventory: 0,
+            itemstypes: [
+                { type: 0, name: "Nix" },
+                { type: 1, name: "Eisen" },
+                { type: 2, name: "schmelzendes Eisen" },
+                { type: 3, name: "Geschmolzendes Eisen" },
+                { type: 4, name: "zerschmolzendes Eisen" },
+                { type: 10, name: "Holz" },
+                { type: 11, name: "Schwert Schaft" },
+                { type: 12, name: "Axe Schaft" },
+                { type: 13, name: "Harke Schaft" },
+                { type: 14, name: "Tool Schaft" },
+                { type: 20, name: "Schwertklinge" },
+                { type: 21, name: "Axeklinge" },
+                { type: 22, name: "Hammerkopf" },
+                { type: 23, name: "Harkekopf" },
+            ],
+            ordertypes: [
+                {
+                    type: 100,
+                    type1: 20,
+                    type2: 11,
+                    name: "Schwert",
+                    time: 2000,
+                },
+                { type: 101, type1: 21, type2: 12, name: "Axe", time: 4000 },
+                { type: 102, type1: 22, type2: 14, name: "Hammer", time: 1000 },
+                { type: 103, type1: 23, type2: 13, name: "Harke", time: 3000 },
+            ],
+            icons: [
+                {
+                    name: "Schwert",
+                    icon:
+                        "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
+                },
+                {
+                    name: "Axe",
+                    icon:
+                        "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
+                },
+                {
+                    name: "Hammer",
+                    icon:
+                        "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
+                },
+                {
+                    name: "Harke",
+                    icon:
+                        "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
+                },
+            ],
+        };
     },
-    deleteorder() {
-      this.orders.shift();
+    methods: {
+        addorder() {
+            this.cnt = this.cnt + 1;
+            let tmp = _.sample(this.ordertypes); //[Math.floor(Math.random()*this.ordertypes.length) | 0];
+            tmp.id = this.cnt;
+            tmp.icon = this.icons.find(
+                (element) => element.name == tmp.name
+            ).icon;
+            this.orders.push(Object.assign({}, tmp));
+            return Object.assign({}, tmp);
+        },
+        deleteorder() {
+            this.orders.shift();
+        },
+        stoporder() {
+            clearInterval(this.intervalid1);
+            //clearTimeout((this.intervalid1);
+        },
+        ordersystem() {
+            let neworder = this.addorder();
+            let min = 2000;
+            let max = 10000;
+            if (this.orders.length > 4) min = 5000;
+            if (this.orders.length > 7) min = 10000;
+            max = max + neworder.time;
+            min = min + neworder.time;
+            let next = Math.floor(Math.random() * (max - min + 1) + min);
+            console.log(next);
+            this.settimeout = setTimeout(
+                function() {
+                    this.ordersystem();
+                }.bind(this),
+                next
+            );
+        },
     },
-    stoporder() {
-      clearInterval(this.intervalid1);
-      //clearTimeout((this.intervalid1);
-    },
-    ordersystem() {
-      let neworder = this.addorder();
-      let min = 2000;
-      let max = 10000;
-      if (this.orders.length > 4) min = 5000;
-      if (this.orders.length > 7) min = 10000;
-      max = max + neworder.time;
-      min = min + neworder.time;
-      let next = Math.floor(Math.random() * (max - min + 1) + min);
-      console.log(next);
-      this.settimeout = setTimeout(
-        function () {
-          this.ordersystem();
-        }.bind(this),
-        next
-      );
-    },
-  },
-  mounted() {
-    this.settimeout = setTimeout(
-      function () {
-        this.ordersystem();
-      }.bind(this),
-      2000
-    );
+    mounted() {
+        this.settimeout = setTimeout(
+            function() {
+                this.ordersystem();
+            }.bind(this),
+            2000
+        );
 
-    this._keyDownListener = function (e) {
-      if (e.key === "w") {
-        e.preventDefault(); // present "Save Page" from getting triggered.
-        this.playerY = this.playerY - 1 < 0 ? 0 : this.playerY - 1;
-      }
-      if (e.key === "s") {
-        e.preventDefault(); // present "Save Page" from getting triggered.
-        this.playerY = this.playerY + 1 > 7 ? 7 : this.playerY + 1;
-      }
-      if (e.key === "a") {
-        e.preventDefault(); // present "Save Page" from getting triggered.
-        this.playerX = this.playerX - 1 < 0 ? 0 : this.playerX - 1;
-      }
-      if (e.key === "d") {
-        e.preventDefault(); // present "Save Page" from getting triggered.
-        this.playerX = this.playerX + 1 > 7 ? 7 : this.playerX + 1;
-      }
-    };
+        this._keyDownListener = function(e) {
+            if (e.key === "w") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerY = this.playerY - 1 < 0 ? 0 : this.playerY - 1;
+            }
+            if (e.key === "s") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerY = this.playerY + 1 > 7 ? 7 : this.playerY + 1;
+            }
+            if (e.key === "a") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerX = this.playerX - 1 < 0 ? 0 : this.playerX - 1;
+            }
+            if (e.key === "d") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerX = this.playerX + 1 > 7 ? 7 : this.playerX + 1;
+            }
+        };
 
-    document.addEventListener("keydown", this._keyDownListener.bind(this));
-  },
-  beforeDestroy() {
-    document.removeEventListener("keydown", this._keyDownListener);
-  },
+        document.addEventListener("keydown", this._keyDownListener.bind(this));
+    },
+    beforeDestroy() {
+        document.removeEventListener("keydown", this._keyDownListener);
+    },
 };
 </script>
--- resources/js/components/gamefield.vue
+++ resources/js/components/gamefield.vue
@@ -1,20 +1,61 @@
 <template>
     <div class="container">
-        <div v-for="y in 8" :key="y - 1" class="row">
-            <div
-                v-for="x in 8"
-                :key="x - 1"
-                :x="x - 1"
-                :y="y - 1"
-                class="fieldTile"
-            >
-                <img
-                    v-if="x - 1 == playerXProp && y - 1 == playerYProp"
-                    src="https://png.pngtree.com/png-clipart/20201212/ourmid/pngtree-car-clipart-small-red-car-png-image_2537665.jpg"
-                    :width="40"
-                    :height="40"
-                />
+        <div class="row">
+            <div class="col">
+                <div class="row justify-content-end">
+                    <img
+                        src="https://img.favpng.com/17/2/25/iron-ingot-metal-clip-art-png-favpng-UJhXuz0HMXciFtc5WckEgx2VD_t.jpg"
+                        height="64px"
+                    />
+                </div>
+                <div class="row justify-content-end">
+                    <img
+                        src="https://previews.123rf.com/images/izakowski/izakowski1401/izakowski140100025/24902150-cartoon-illustration-of-wooden-plank-or-board-clip-art.jpg"
+                        height="64px"
+                    />
+                </div>
             </div>
+            <div class="col">
+                <div v-for="y in 8" :key="y - 1" class="row">
+                    <div
+                        v-for="x in 8"
+                        :key="x - 1"
+                        :x="x - 1"
+                        :y="y - 1"
+                        class="fieldTile"
+                    >
+                        <img
+                            v-if="x - 1 == playerXProp && y - 1 == playerYProp"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3jADMM1UoiT6at21ewy4p5S1VcKtDTaXVLQ&usqp=CAU"
+                            :width="32"
+                            :height="32"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row justify-content-start">
+                    <img
+                        src="https://media.istockphoto.com/vectors/traditional-woodworking-workshop-vector-id493281662?k=6&m=493281662&s=612x612&w=0&h=kBP0BAu8r8c51225b_GNCs0igGXfmHCpcGut-J8tltM="
+                        height="64px"
+                    />
+                </div>
+                <div class="row justify-content-start">
+                    <img
+                        src="https://d12oja0ew7x0i8.cloudfront.net/images/Article_Images/ImageForArticle_17588(1).jpg"
+                        height="64px"
+                    />
+                </div>
+                <div class="row justify-content-start">
+                    <img
+                        src="https://images-na.ssl-images-amazon.com/images/I/81mVeFWTerL._AC_SY355_.jpg"
+                        height="80px"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <span>Inventory: {{ playerInventoryProp }}</span>
         </div>
     </div>
 </template>
@@ -26,6 +67,7 @@ export default {
     props: {
         playerXProp: Number,
         playerYProp: Number,
+        playerInventoryProp: Number,
     },
     data: () => {
         return {
@@ -43,8 +85,8 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 40px;
-    height: 40px;
-    border: 1px solid black;
+    width: 32px;
+    height: 32px;
+    border: 1px solid lightgrey;
 }
 </style>
