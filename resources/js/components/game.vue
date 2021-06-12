26818d85d30c05f6de914a33f84f5b6c3425ea9d
Alexander Eichhorn <alex@code-with.us>
2021-06-12 15:11:54 +0200

    gamefield

--- resources/js/components/game.vue
+++ resources/js/components/game.vue
@@ -1,22 +1,47 @@
 <template>
-  <orders></orders>
+<div>
+  <orders
+  :ordersProp="orders"
+  ></orders>
+  <gamefield></gamefield>
+</div>
 </template>
 
 <script>
     export default {
+  		components: {},
 		name: "game",
 		inheritAttrs: false,
 		props: {
 		},
 		data: () => {
 			return {
+				orders: [],
+				cnt: 1,
+				intervalid1: false,
 			}
 		},
 		methods:
 		{
+			addorder()
+			{
+				this.cnt = this.cnt+1;
+				this.orders.push({'id': this.cnt, 'name': 'schwert', 'icon': 'https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836'});	
+			},
+			deleteorder()
+			{
+				this.orders.shift();
+			},
+			stoporder()
+			{
+				clearInterval(this.intervalid1);
+				//clearTimeout((this.intervalid1);
+			},
 		},
 		mounted() {
-				
+				this.intervalid1 = setInterval(function(){
+					this.addorder()
+				}.bind(this), 5000);
 			}
 	}
 </script>
--- resources/js/components/orders.vue
+++ resources/js/components/orders.vue
@@ -4,14 +4,11 @@
       <div class="col">
         <div
           id="orders"
-          v-for="order in this.orders"
+          v-for="order in this.ordersProp"
           :key="order.id"
         >
 		<img :src="order.icon" height="100px" width="100px"/>
 		</div>
-		<button @click="addorder()" name="add" value="add">Add</button>
-		<button @click="deleteorder()" name="delete" value="delete">delete</button>
-		<button @click="stoporder()" name="stop" value="stop">stop</button>
       </div>
     </div>
   </div>
@@ -22,36 +19,17 @@
 		name: "orders",
 		inheritAttrs: false,
 		props: {
+			ordersProp: Array,
 		},
 		data: () => {
 			return {
-			orders: [],
-			cnt: 1,
-			intervalid1: false,
+			
 			}
 		},
 		methods:
 		{
-			addorder()
-			{
-				this.cnt = this.cnt+1;
-				this.orders.push({'id': this.cnt, 'name': 'schwert', 'icon': 'https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836'});
-			},
-			deleteorder()
-			{
-				this.orders.shift();
-			},
-			stoporder()
-			{
-				clearInterval(this.intervalid1);
-				//clearTimeout((this.intervalid1);
-			},
 		},
 		mounted() {
-				console.log("Mounted");
-				this.intervalid1 = setInterval(function(){
-					this.addorder()
-				}.bind(this), 5000);
-			}
+		}
 	}
 </script>
