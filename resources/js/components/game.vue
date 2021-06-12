9b1ba174111d5c0b01c7717fed57cdee59fc24d8
Michael Matthé <michael.matthe@gmail.com>
2021-06-12 16:11:45 +0200

    add game field grid

--- resources/js/components/game.vue
+++ resources/js/components/game.vue
@@ -1,47 +1,104 @@
 <template>
-<div>
-  <orders
-  :ordersProp="orders"
-  ></orders>
-  <gamefield></gamefield>
-</div>
+    <div @keyup="onKeyPress">
+        <orders :ordersProp="orders"></orders>
+        <gamefield :playerXProp="playerX" :playerYProp="playerY"></gamefield>
+    </div>
 </template>
 
 <script>
-    export default {
-  		components: {},
-		name: "game",
-		inheritAttrs: false,
-		props: {
-		},
-		data: () => {
-			return {
-				orders: [],
-				cnt: 1,
-				intervalid1: false,
-			}
-		},
-		methods:
-		{
-			addorder()
-			{
-				this.cnt = this.cnt+1;
-				this.orders.push({'id': this.cnt, 'name': 'schwert', 'icon': 'https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836'});	
-			},
-			deleteorder()
-			{
-				this.orders.shift();
-			},
-			stoporder()
-			{
-				clearInterval(this.intervalid1);
-				//clearTimeout((this.intervalid1);
-			},
-		},
-		mounted() {
-				this.intervalid1 = setInterval(function(){
-					this.addorder()
-				}.bind(this), 5000);
-			}
-	}
+export default {
+    components: {},
+    name: "game",
+    inheritAttrs: false,
+    props: {},
+    data: () => {
+        return {
+            orders: [],
+            cnt: 1,
+            intervalid1: false,
+            playerX: 0,
+            playerY: 0,
+            keyPressed: false,
+        };
+    },
+    methods: {
+        addorder() {
+            this.cnt = this.cnt + 1;
+            this.orders.push({
+                id: this.cnt,
+                name: "schwert",
+                icon:
+                    "https://scratchwars.com/photos/zbrane_avatar/f/0/9.png?m=1485025836",
+            });
+        },
+        deleteorder() {
+            this.orders.shift();
+        },
+        stoporder() {
+            clearInterval(this.intervalid1);
+            //clearTimeout((this.intervalid1);
+        },
+        onKeyPress(event) {
+            console.log(event);
+            //if(event.which ==
+        },
+    },
+    mounted() {
+        this.intervalid1 = setInterval(
+            function() {
+                this.addorder();
+            }.bind(this),
+            500
+        );
+
+        this._keyDownListener = function(e) {
+            if (this.keyPressed) {
+                return;
+            }
+            if (e.key === "w") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerY = this.playerY - 1 < 0 ? 0 : this.playerY - 1;
+            }
+            if (e.key === "s") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerY = this.playerY + 1 > 7 ? 7 : this.playerY + 1;
+            }
+            if (e.key === "a") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerX = this.playerX - 1 < 0 ? 0 : this.playerX - 1;
+            }
+            if (e.key === "d") {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.playerX = this.playerX + 1 > 7 ? 7 : this.playerX + 1;
+            }
+            if (
+                e.key === "w" ||
+                e.key === "s" ||
+                e.key === "a" ||
+                e.key === "d"
+            ) {
+                this.keyPressed = true;
+            }
+        };
+
+        this._keyUpListener = function(e) {
+            if (
+                e.key === "w" ||
+                e.key === "s" ||
+                e.key === "a" ||
+                e.key === "d"
+            ) {
+                e.preventDefault(); // present "Save Page" from getting triggered.
+                this.keyPressed = false;
+            }
+        };
+
+        document.addEventListener("keydown", this._keyDownListener.bind(this));
+        document.addEventListener("keyup", this._keyUpListener.bind(this));
+    },
+    beforeDestroy() {
+        document.removeEventListener("keydown", this._keyDownListener);
+        document.addEventListener("keyup", this._keyUpListener.bind(this));
+    },
+};
 </script>
--- resources/js/components/gamefield.vue
+++ resources/js/components/gamefield.vue
@@ -1,54 +1,43 @@
 <template>
-  <div class="container xy">
-	  <div class="x0 y0" data-x='0' data-y='0'></div>
-	  <div class="x1 y0" data-x='1' data-y='0'></div>
-	  <div class="x2 y0" data-x='2' data-y='0'></div>
-	  <div class="x3 y0" data-x='3' data-y='0'></div>
-	  <div class="x4 y0" data-x='4' data-y='0'></div>
-	  <div class="x5 y0" data-x='5' data-y='0'></div>
-	  <div class="x0 y1" data-x='0' data-y='1'></div>
-	  <div class="x0 y2" data-x='0' data-y='2'></div>
-	  <div class="x0 y3" data-x='0' data-y='3'></div>
-	  <div class="x0 y4" data-x='0' data-y='4'></div>
-	  <div class="x0 y5" data-x='0' data-y='5'></div>
-  </div>
+    <div class="container">
+        <div v-for="y in 8" :key="y - 1" class="row">
+            <div
+                v-for="x in 8"
+                :key="x - 1"
+                :x="x - 1"
+                :y="y - 1"
+                class="fieldTile"
+            >
+                <span v-if="x - 1 == playerXProp && y - 1 == playerYProp"
+                    >X</span
+                >
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-		name: "gamefield",
-		inheritAttrs: false,
-		props: {
-		},
-		data: () => {
-			return {
-			
-			}
-		},
-		methods:
-		{
-		},
-		mounted() {
-		}
-	}
+export default {
+    name: "gamefield",
+    inheritAttrs: false,
+    props: {
+        playerXProp: Number,
+        playerYProp: Number,
+    },
+    data: () => {
+        return {
+            fieldWidth: 10,
+            fieldHeight: 10,
+        };
+    },
+    methods: {},
+    mounted() {},
+};
 </script>
 
 <style scoped>
-.xy {
-	display: grid;
-	grid-template-columns: repeat(5, 1fr);
-	grid-template-rows: repeat(5, 1fr);
-	grid-column-gap: 0px;
-	grid-row-gap: 0px;
-}
-.x0 {
-	background: lightblue;
-	height: 10px;
-	width: 10px;
-}
-.y0 {
-	background: lightcoral;
-	height: 10px;
-	width: 10px;
+.fieldTile {
+    width: 40px;
+    height: 40px;
 }
 </style>
